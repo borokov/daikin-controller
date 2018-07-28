fa7a3aa71d09079577eea56e6cedb278fe144e92
pyros <tandrieu@protonmail.com>
2018-07-28 11:24:41 +0200

    Check return code in DaikinHTTPController.

--- app/src/main/java/com/example/pyros/daikin_controller/DaikinHTTPController.java
+++ app/src/main/java/com/example/pyros/daikin_controller/DaikinHTTPController.java
@@ -100,18 +100,20 @@ public class DaikinHTTPController
             wr.close();
 
             int responseCode = connection.getResponseCode();
-
-            //Get Response
-            InputStream is = connection.getInputStream();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-            String line;
-            StringBuffer response = new StringBuffer();
-            while ((line = rd.readLine()) != null)
-            {
-                response.append(line);
-                response.append('\r');
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                //Get Response
+                InputStream is = connection.getInputStream();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+                String line;
+                StringBuffer response = new StringBuffer();
+                while ((line = rd.readLine()) != null) {
+                    response.append(line);
+                    response.append('\r');
+                }
+                rd.close();
+            } else {
+                Log.e("sendGet", "POST Error:" + responseCode);
             }
-            rd.close();
 
         } catch (Exception e) {
 
@@ -140,14 +142,17 @@ public class DaikinHTTPController
             connection.setRequestMethod("GET");
 
             int responseCode = connection.getResponseCode();
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
 
-            while ((inputLine = in.readLine()) != null)
-            {
-                response.append(inputLine);
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+            } else {
+                Log.e("sendGet", "GET Error:" + responseCode);
             }
-            in.close();
 
         } catch (Exception e) {
 
